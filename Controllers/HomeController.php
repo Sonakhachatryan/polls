f74b11b115ff51745b063c3d4a104ced01f1c7fc
davmnatsakanyan1990 <dav.mnatsakanyan@gmail.com>
2017-02-17 01:02:16 +0400

    pdo

--- Controllers/HomeController.php
+++ Controllers/HomeController.php
@@ -1,18 +1,21 @@
 <?php
 
-/**
- * Created by PhpStorm.
- * User: Designer
- * Date: 04.10.2016
- * Time: 12:45
- */
+
+
+
 class HomeController
 {
 
     public function index(){
-//        echo $id;
-//        echo $name;
-        echo 'home controller';
+        $db = new DB();
+
+        $data = $db->table('users')->where('email', 'dav.mnatsakanyan@gmail.com')->get();
+var_dump($data);
+//        foreach ($data as $d) {
+//            print_r($d);
+//        }
+
+
 
     }
 }
